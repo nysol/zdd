c658543be64f7abded2ff3f780a15e378332df1e
nain <nain@nain.local>
2025-05-28 22:32:01 +0900

    extconf追加

--- ext/zdd_so/extconf.rb
+++ ext/zdd_so/extconf.rb
@@ -1,17 +1,17 @@
 require 'mkmf'
 cp = "$(srcdir)"
-$CFLAGS = " -O3 -Wall -I. -I#{cp}/include -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing"
-$CPPFLAGS = " -O3 -Wall -I. -I#{cp}/include  -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing"
-$CXXFLAGS = " -O3 -Wall -I. -I#{cp}/include  -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing"
+$CFLAGS = " -O3 -Wall -I. -I#{cp}/include -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing -Wno-implicit-function-declaration -Wno-reserved-user-defined-literal"
+$CPPFLAGS = " -O3 -Wall -I. -I#{cp}/include  -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing -Wno-implicit-function-declaration -Wno-reserved-user-defined-literal"
+$CXXFLAGS = " -O3 -Wall -I. -I#{cp}/include  -DB_STATIC -D_NO_MAIN_ -DLINE -fPIC -Wno-error=format-security -Wno-narrowing -Wno-implicit-function-declaration -Wno-reserved-user-defined-literal"
 
 
 $LOCAL_LIBS += " -lstdc++"
 
 if RUBY_VERSION >= '2.0.0' then
 	if Gem::Platform::local.os =~ /darwin/ then
-	$CFLAGS += " -Wno-error=unused-command-line-argument-hard-error-in-future -Wno-implicit-function-declaration -Wno-reserved-user-defined-litera"
-	$CPPFLAGS += " -Wno-error=unused-command-line-argument-hard-error-in-future -Wno-implicit-function-declaration -Wno-reserved-user-defined-litera"
-	$LOCAL_LIBS += " -Wno-error=unused-command-line-argument-hard-error-in-future -Wno-implicit-function-declaration -Wno-reserved-user-defined-litera"
+	$CFLAGS += " -Wno-error=unused-command-line-argument-hard-error-in-future"
+	$CPPFLAGS += " -Wno-error=unused-command-line-argument-hard-error-in-future"
+	$LOCAL_LIBS += " -Wno-error=unused-command-line-argument-hard-error-in-future"
 	end
 end
 
